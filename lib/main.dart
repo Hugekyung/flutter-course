435953b6034c8d02c82973d86fa2c684d3b86f24
haechan <haechan2000@noutecompany.com>
2023-08-20 01:19:58 +0900

    feat: [3강]Stack으로 위젯 여러개를 중첩하는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,38 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('test'),
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.yellow,
+        child: Stack(
+          // ! Stack으로 위젯을 중첩할 수 있다.
+          // * 아래는 계단식 중첩
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(
+                top: 50,
+                left: 50,
+              ),
+              color: Colors.red,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(
+                top: 100,
+                left: 100,
+              ),
+              color: Colors.green,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
