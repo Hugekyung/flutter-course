cf6eba28d69c35fb095e7712a3e450669a869e38
haechan <haechan2000@noutecompany.com>
2023-08-19 22:00:23 +0900

    feat: [1강]flutter 초기 세팅, Scaffold 뼈대 구성

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header Part'),
+            ),
+            ListTile(
+              title: Text('Menu 1'),
+            )
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text('Hello world'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('clicked'),
+        child: const Icon(Icons.mouse),
+      ),
+    );
+  }
+}
